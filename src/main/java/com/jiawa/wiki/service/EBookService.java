94d5b9a2703b59806796dc16ad0fdb8f2a093ba2
yuu <mingyangyu707@gmail.com>
2021-06-28 19:25:54 +0800

    14.增加CopyUtil封装BeanUtils

--- src/main/java/com/jiawa/wiki/service/EBookService.java
+++ src/main/java/com/jiawa/wiki/service/EBookService.java
@@ -3,13 +3,12 @@ package com.jiawa.wiki.service;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.po.Ebook;
 import com.jiawa.wiki.po.EbookExample;
+import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.vo.req.EBookVo;
 import com.jiawa.wiki.vo.resp.EBookRespVo;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,12 +24,16 @@ public class EBookService {
         EbookExample ebookExample = new EbookExample();
         ebookExample.createCriteria().andNameLike("%"+req.getName()+"%");
         List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
-        ArrayList<EBookRespVo> resplist = new ArrayList<>();
+        /*ArrayList<EBookRespVo> resplist = new ArrayList<>();
         for (Ebook ebook : ebooks) {
             EBookRespVo respVo = new EBookRespVo();
             BeanUtils.copyProperties(ebook, respVo);
+            //对象复制
+            EBookRespVo respVo = CopyUtil.copy(ebook, EBookRespVo.class);
             resplist.add(respVo);
-        }
-        return resplist;
+        }*/
+        //列表复制
+        List<EBookRespVo> respList = CopyUtil.copyList(ebooks, EBookRespVo.class);
+        return respList;
     }
 }
