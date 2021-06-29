b7519428d8cca96048bb6981c929eef0b55ed77d
yuu <mingyangyu707@gmail.com>
2021-06-29 12:03:17 +0800

    31.配置Axios拦截器打印请求和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -11,6 +11,24 @@ axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app')
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    const response = error.response;
+    return Promise.reject(error);
+});
+
 //全局使用图标
 const icons: any = Icons;
 for (const i in icons) {
