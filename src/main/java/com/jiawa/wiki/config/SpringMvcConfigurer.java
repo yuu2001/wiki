9da10c5976df022a845515e35cc632ca06279063
yuu <mingyangyu707@gmail.com>
2021-06-29 17:34:00 +0800

    33.增加拦截器打印接口耗时

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfigurer.java
@@ -0,0 +1,26 @@
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author yuu
+ **/
+@Configuration
+public class SpringMvcConfigurer implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+
+}
